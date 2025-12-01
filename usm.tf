deedf0b7a7675c85bf094e6687b3004ef93f4264
Sven Erik Knop <seknop@gmail.com>
2025-12-01 14:19:57 +0000

    Add USM Manager as a service user with the correct roles.

---
+++ usm.tf
@@ -0,0 +1,17 @@
+resource "confluent_service_account" "usm-manager" {
+  display_name = var.usm-manager
+  description  = "Service account to manage USM agents"
+}
+
+resource "confluent_role_binding" "usm-manager-usm-agent" {
+  principal   = "User:${confluent_service_account.usm-manager.id}"
+  role_name   = "UsmAgent"
+  crn_pattern = confluent_environment.usm_environment.resource_name
+}
+
+resource "confluent_role_binding" "usm-manager-usm-agent" {
+  principal   = "User:${confluent_service_account.usm-manager.id}"
+  role_name   = "DataSteward"
+  crn_pattern = confluent_environment.usm_environment.resource_name
+}
+
